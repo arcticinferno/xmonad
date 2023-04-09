63b137340dd0ee8564189bc08e02e6413e632989
Arctic Inferno <arcticinferno@protonmail.com>
2023-04-09 16:28:46 -0400

    Add keybinds

--- xmonad.hs
+++ xmonad.hs
@@ -4,13 +4,31 @@
 import XMonad
 
 -- -- Util
-import XMonad.Util.EZConfig ()
+import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Util.Ungrab ()
 
 --[[ 
 --  -> SETTINGS
 --]]
-xMod = mod4Mask
+xMod :: KeyMask
+xMod = mod1Mask -- Alt
+
+xEditor :: String
+xEditor = "emacsclient -c -a 'emacs'"
+
+xBrowser :: String
+xBrowser = "firefox"
+
+xTerminal :: String
+xTerminal = "alacritty"
+
+xConfig :: XConfig (Choose Tall (Choose (Mirror Tall) Full))
+xConfig =
+  def {modMask = xMod} `additionalKeysP`
+  [ ("M-S-u", spawn xEditor)
+  , ("M-u", spawn xBrowser)
+  , ("M-S-<Return>", spawn xTerminal)
+  ]
 
 --[[
 --  -> MAIN
