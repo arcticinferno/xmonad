3de88d79f2e5ef161b050aa9c48c49b8818782b2
arcticinferno <nil>
2023-04-22 21:56:20 -0400

    update workspaces

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,3 @@
---[[
---  -> IMPORTS
---]]
 import XMonad
 
 -- General
@@ -26,9 +23,6 @@ import XMonad.Layout.Spacing
 -- System
 import System.IO
 
---  -> SETTINGS
---]]
---
 type Keybind = (String, X ())
 
 (<:>) :: String -> String -> Keybind
@@ -40,14 +34,28 @@ xMod = mod1Mask -- Alt
 xBrowser :: Keybind
 xBrowser = "M-u" <:> "firefox-bin"
 
+xScreenshot :: Keybind
+xScreenshot = "M-h" <:> "flameshot gui"
+
 xTerminal :: Keybind
 xTerminal = "M-S-<Return>" <:> "alacritty"
 
 xLauncher :: Keybind
 xLauncher = "M-p" <:> "rofi -show drun"
 
+switchLang :: Keybind
+switchLang = "M-v" <:> "~/.local/bin/switchlang"
+
 xMusic :: Keybind
 xMusic = "M-S-u" <:> "alacritty -e \"ncmpcpp\""
+
+xDoc :: Keybind
+xDoc =
+  "M-S-f" <:>
+  "zathura ~/doc/$(/bin/ls ~/doc | rofi -dmenu -p \"Open: \") || return"
+
+xEditFile :: Keybind
+xEditFile = "M-f" <:> "~/.local/bin/fx"
 
 xLayout =
   avoidStruts $
@@ -55,9 +63,6 @@ xLayout =
   spacingRaw True (Border 10 10 10 10) True (Border 10 10 10 10) True $
   layoutHook def
 
---[[
---  -> MAIN
---]]
 main = do
   xmproc0 <- spawnPipe "xmobar -x 0 ~/.xmobarrc -d"
   xmproc1 <- spawnPipe "xmobar -x 1 ~/.xmobarrc -d"
@@ -68,23 +73,42 @@ main = do
       { terminal = "alacritty"
       , borderWidth = 2
       , normalBorderColor = "#cccccc"
+      , workspaces =
+          [ " λ-dev "
+          , " λ-sys "
+          , " λ-music "
+          , " λ-www "
+          , " λ-vid "
+          , " λ-doc "
+          , " λ-mvs "
+          , " λ-kvm "
+          , " λ-game "
+          ]
       , focusedBorderColor = "#cd8b00"
       , manageHook = manageDocks <+> manageHook def
       , layoutHook = avoidStruts $ xLayout -- Add spacing with 10 pixels
       , logHook =
-          dynamicLogWithPP
-            xmobarPP
-              { ppOutput =
-                  \x ->
-                    hPutStrLn xmproc0 x >> hPutStrLn xmproc1 x >>
-                    hPutStrLn xmproc2 x
-              , ppTitle = xmobarColor "green" "" . shorten 50
-              , ppCurrent = xmobarColor "#429942" "" . wrap "[" "]"
-              , ppVisible = xmobarColor "#5b6ea7" "" . wrap "(" ")"
-              , ppHidden = xmobarColor "#4c7899" ""
-              , ppHiddenNoWindows = xmobarColor "#4c7899" "" . wrap "-" "-"
-              , ppSep = "   "
-              , ppWsSep = " "
-              }
+          do let ppOutput' x =
+                   hPutStrLn xmproc0 x >> hPutStrLn xmproc1 x >>
+                   hPutStrLn xmproc2 x
+             dynamicLogWithPP
+               xmobarPP
+                 { ppOutput = ppOutput'
+                 , ppTitle = xmobarColor "green" "" . shorten 50
+                 , ppCurrent = xmobarColor "#ff6b6b" "" . id
+                 , ppVisible = xmobarColor "#ff9ff3" "" . id
+                 , ppHidden = xmobarColor "#80c8ff" "" . id
+                 , ppHiddenNoWindows = xmobarColor "#80c8ff" "" . id
+                 , ppSep = "   "
+                 , ppWsSep = " "
+                 }
       } `additionalKeysP`
-    [xBrowser, xLauncher, xTerminal, xMusic]
+    [ xBrowser
+    , xLauncher
+    , xTerminal
+    , xMusic
+    , xDoc
+    , xScreenshot
+    , xEditFile
+    , switchLang
+    ]
